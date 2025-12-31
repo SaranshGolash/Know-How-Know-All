574001bd246a1d87714c33de47c2adbd1718b16d
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2025-12-31 21:34:50 +0530

    Created a static header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import Header from "./Header";
 
-function App() {}
+function App() {
+  <Header />;
+}
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,34 @@
 import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div className="navBar">
+        <h4>Know-How-Know-All</h4>
+        <span>services</span>
+        <button className="nav-btn" id="login">
+          Login
+        </button>
+        <button className="nav-btn" id="signup">
+          SignUp
+        </button>
+      </div>
+      <div className="hero-section">
+        <div className="hero-section-context">
+          <h1>Learn, Perform, Compete, and Hustle</h1>
+          <span>
+            A E-Learning Platform having 0 human teachers. You heard it right!
+            Our AI will be tutoring you all across various domains and skills.
+            This platform is first of its kind
+          </span>
+          <button className="hero-section-btn">Check out the domains</button>
+        </div>
+        <div className="hero-section-img">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
