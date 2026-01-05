e7db5acc1c623834cc3ce56bb8423733ff70343e
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2026-01-05 07:23:16 +0530

    Started creating Login Component

---
+++ src/components/Login.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Login() {
+  const styles = {
+    loginSection: {},
+  };
+  return (
+    <div className="loginSection">
+      <div className="loginText">
+        <div className="companyName">
+          <span>Know-How-Know-Where</span>
+        </div>
+        <span>Nice to meet you again</span>
+        <h1>Welcome Back!</h1>
+      </div>
+      <div className="loginForm">
+        <h3>Login Account</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
